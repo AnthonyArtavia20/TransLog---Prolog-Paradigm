b772089d8ae5474c92988c11945bf0cb2df707f6
Alex21FW <alexeduardofengwu@gmail.com>
2025-10-23 00:37:43 -0600

    se arreglo lo de los plurales

--- Data_Base.pl
+++ Data_Base.pl
@@ -1,4 +1,4 @@
-% Base de datos de traducciones espaÃ±ol-inglÃ©s
+% Base de datos de traducciones espaï¿½ol-inglï¿½s
 % Numeros
 trad("uno", "one").
 trad("dos", "two").
@@ -23,20 +23,29 @@ trad("ciento", "onehundred").
 trad("once","eleven").
 trad("doce", "twelve").
 trad("trece", "thirteen").
+trad("catorce", "fourteen").
+trad("quince", "fifteen").
+trad("dieciseis", "sixteen").
+trad("diecisiete", "seventeen").
+trad("dieciocho", "eighteen").
+trad("diecinueve", "nineteen").
 trad("docientos","twohundred").
 
 trad("hola","hi").
 trad("yo", "i").
 trad("soy","am").
 trad("ella","she").
-trad("Ã©l","he").
+trad("él","he").
 trad("es","is").
 trad("son","are").
+trad("comen", "eats").
 
 trad("y", "and").
 
 trad("el","the").
 trad("la", "the").
+trad("las", "the").
+trad("los", "the").
 
 trad("amigos","friends").
 trad("amigo","friend").
@@ -45,38 +54,51 @@ trad("carro","car").
 
 trad("negro","black").
 
-trad("niÃ±os","kids").
-trad("niÃ±o","kid").
+trad("niños","kids").
+trad("niño","kid").
 trad("come","eats").
 trad("manzana","apple").
+trad("manzanas", "apples").
 
 %sujetos o nombres
-nom("niÃ±o").
-nom("niÃ±os").
+nom("niño").
+nom("niños").
 nom("kid").
 nom("kids").
 nom("manzana").
+nom("manzanas").
 nom("apple").
+nom("apples").
 nom("amigos").
 nom("amigas").
+nom("él").
+nom("gato").
+nom("carro").
 
 %verbos
 verb("come").
 verb("eats").
+verb("comen").
+verb("eat").
 
 %determinantes
 deter("el").
 deter("the").
 deter("la").
+deter("las").
+deter("los").
 
 %femenino
 fem("manzana").
 fem("amigas").
 fem("apple").
+fem("manzanas").
+fem("apples").
 
 %masculino
 mas("gato").
 mas("amigos").
+mas("niños").
 
 
 %palabras en singular
@@ -92,9 +114,15 @@ plur("are").
 plur("amigos").
 plur("friends").
 plur("kids").
-plur("niÃ±os").
+plur("niños").
+plur("manzanas").
+plur("apples").
+plur("comen").
+plur("los").
+plur("eats").
 
-% Lista completa de nÃºmeros reconocidos
+
+% Lista completa de nï¿½meros reconocidos
 num("uno").
 num("dos").
 num("tres").
@@ -105,6 +133,15 @@ num("siete").
 num("ocho").
 num("nueve").
 num("diez").
+num("once").
+num("doce").
+num("trece").
+num("catorce").
+num("quince").
+num("dieciseis").
+num("diecisiete").
+num("dieciocho").
+num("diecinueve").
 num("veinte").
 num("treinta").
 num("cuarenta").
@@ -124,6 +161,15 @@ num("seven").
 num("eight").
 num("nine").
 num("ten").
+num("eleven").
+num("twelve").
+num("thirteen").
+num("fourteen").
+num("fifteen").
+num("sixteen").
+num("seventeen").
+num("eighteen").
+num("nineteen").
 num("twenty").
 num("thirty").
 num("forty").
@@ -149,6 +195,6 @@ decena("onehundred").
 
 % Lenguajes soportados
 lenguaje("ingles", 1).
-lenguaje("espaÃ±ol", 2).
+lenguaje("español", 2).
 lenguaje("english", 1).
 lenguaje("spanish", 2).
